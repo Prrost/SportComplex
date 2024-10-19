499704d1d047b976cb89c34870a37efa2c77186b
Prrost <prrostik@gmail.com>
2024-10-19 18:31:06 +0500

    first two

--- scripts/rostislavjs.js
+++ scripts/rostislavjs.js
@@ -16,3 +16,42 @@ function currentTime(){
 }
 
 setInterval(currentTime, 1000)
+
+function resetForm(){
+    document.querySelectorAll('input').forEach(input => input.value = '')
+}
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    //ButtonEventListener
+    document.getElementById('resetButton').addEventListener('click', function () {
+        document.querySelectorAll('input').forEach(input => input.value = '');
+    });
+
+
+    //KeyboardEventListener
+    const menuItems = [...document.querySelectorAll('#navbarMenu .nav-link'),
+        document.getElementById('subscribeButton'),
+        document.getElementById('homeButton')];
+    let currentIndex = 0;
+
+    menuItems[currentIndex].focus();
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+            currentIndex = (currentIndex + 1) % menuItems.length;
+            menuItems[currentIndex].focus();
+            event.preventDefault();
+        }
+
+        else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
+            currentIndex = (currentIndex - 1 + menuItems.length) % menuItems.length;
+            menuItems[currentIndex].focus();
+            event.preventDefault();
+        }
+    });
+});
